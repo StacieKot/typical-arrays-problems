b7ba00243892e98832308d5a3da2f4d67a39c600
StacieKot <stacie.kot@gmail.com>
2021-03-04 20:57:39 +0300

    feat: develop 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+   array = array || [];
+   if(array[0]!==undefined){
+      array.sort((a, b) => a - b )
+      return array[0];
+   }
+   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+   array = array || [];
+   if(array[0]!==undefined){
+      array.sort((a, b) => b - a);
+      return array[0];
+   }
+   return 0;
+
 }
 
 exports.avg = function avg (array) {
+  array = array || [];
+  if(array[0]!==undefined){
+     let sum = array.reduce((r,v) => r+=v);
+     return (sum/array.length);
+  }
   return 0;
 }
